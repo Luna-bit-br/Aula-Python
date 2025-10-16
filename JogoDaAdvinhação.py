203d12abc77039ebf7cca9314588ae5766d7b934
Luna-bit-br <pedrohluna2002@gmail.com>
2025-10-16 17:02:11 -0300

    Jogo da Advinhação Aula 16/10/2025

---
+++ JogoDaAdvinhação.py
@@ -0,0 +1,19 @@
+import random
+
+numero_secreto = random.randint(1, 10)
+
+chute = int(input("Advinhe qual o número de 0 à 10!"))
+
+if chute == numero_secreto:
+    print("Parabéns você acertou!")
+elif chute > numero_secreto:
+    print("Você errou! O número secreto era menor!")
+elif chute < numero_secreto:
+    print("Você errou! O número secreto era maior!")
+
+segundo_chute = int(input("Segunda tentativa para advinhar qual o número de 0 à 10!"))
+
+if segundo_chute == numero_secreto:
+    print("Você Acertou!")
+else:
+    print(f"Você errou! O número era {numero_secreto}")
